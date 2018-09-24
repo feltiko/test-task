df5a21bdcd832609dc541640b4879bcf8abd5971
Luckianchikov Max <feltmax@gmail.com>
2018-09-24 08:18:56 +0600

    Change primary color

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
+import theme from './theme';
 import './index.css';
 import App from './App';
 import store from './Store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
