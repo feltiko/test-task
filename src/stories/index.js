d7cdbf1843680e2ac758c77936b6bcd000a06518
Luckianchikov Max <feltmax@gmail.com>
2019-03-18 13:15:45 +0600

    Setup a stories for storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,27 +2,20 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import { Provider } from 'react-redux';
 
 import Modal from '../Components/Modal';
+import Item from '../Components/Item';
 import store from '../Store';
-
-import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Modal', module).add('Default modal', () => (
   <Provider store={store}>
-    <Modal isActive={true}  />
+    <Modal handleClose={action('closePopup')} isActive={true} />
   </Provider>
 ));
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('Modal', module).add('Modal item', () => (
+  <Provider store={store}>
+    <Item removeHandler={action('removeItem')} data={{ select: 'Once', number: 322 }} />
+  </Provider>
+));
